6ab4ed644322166b910248299f0bbcd79f9d9850
bigsexy <luispayan2001@gmail.com>
2023-07-02 14:22:14 -0600

    Setting up vercel

--- server/src/services/mongo.js
+++ server/src/services/mongo.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-const MONGO_URL = process.env.MONGO_URL;
+const MONGO_URI = process.env.MONGO_URI;
 mongoose.connection.once('open', () => {
   console.log('MongoDB connection ready!');
 });
@@ -11,7 +11,7 @@ mongoose.connection.on('error', (err) => {
 });
 
 async function mongoConnect() {
-  await mongoose.connect(MONGO_URL);
+  await mongoose.connect(MONGO_URI);
 }
 
 async function mongoDisconnect() {
