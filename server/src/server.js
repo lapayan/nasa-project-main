871eb46a08d9870a3f54dabcdd494cd0b033d6f4
bigsexy <luispayan2001@gmail.com>
2023-07-04 19:04:43 -0600

    Temp changes

--- server/src/server.js
+++ server/src/server.js
@@ -7,7 +7,7 @@ const { mongoConnect } = require('./services/mongo');
 const { loadPlanetsData } = require('./models/planets.model');
 const { loadLaunchData } = require('./models/launches.model');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 443;
 
 const server = https.createServer({
   key: process.env.HTTPS_KEY, 
